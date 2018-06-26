7908dc4cae0ffccd2ed80ca7a3af1af88c1dec87
IcyMat <icymat@outlook.com>
2018-06-26 18:16:07 +0200

    Set ArrayCollection as iterable

--- ArrayCollection.php
+++ ArrayCollection.php
@@ -6,94 +6,153 @@
 
 namespace IcyMat\ORM;
 
-class ArrayCollection {
+class ArrayCollection implements \Iterator {
 
-	/**
-	 * @var array
-	 */
-	protected $array;
+    /**
+     * @var array
+     */
+    protected $array;
 
-	/**
-	 * ArrayCollection constructor.
-	 * @param array $array
-	 */
-	public function __construct(array $array = array()) {
-		$this->array = $this->child($array);
-	}
+    /**
+     * @var array
+     */
+    protected $keys = [];
 
-	/**
-	 * @param $array
-	 * @return array
-	 */
-	private function child($array) {
-		if(!empty($array))
-			foreach($array as $index => $value) {
-				if(is_array($value)) {
-					$array[$index] = new self($value);
-				}
-			}
+    /**
+     * @var int
+     */
+    private $iteratrionPosition = 0;
 
-		return $array;
-	}
+    /**
+     * ArrayCollection constructor.
+     * @param array $array
+     */
+    public function __construct(array $array = array()) {
+        $this->array = $this->child($array);
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString() {
-		return '<pre>'.print_r($this->array, true).'</pre>';
-	}
+    /**
+     * @param $array
+     * @return array
+     */
+    private function child($array) {
+        if(!empty($array))
+            foreach($array as $index => $value) {
+                if(is_array($value)) {
+                    $array[$index] = new self($value);
+                }
+            }
 
-	/**
-	 * @param $index
-	 * @return mixed|null
-	 */
-	public function get($index) {
-		if(array_key_exists($index, $this->array))
-			return $this->array[$index];
-		else
-			return null;
-	}
+        return $array;
+    }
 
-	/**
-	 * @param $index
-	 * @param $value
-	 * @return $this
-	 */
-	public function set($index, $value) {
-		$this->array[$index] = $value;
+    /**
+     * @return string
+     */
+    public function __toString() {
+        return '<pre>'.print_r($this->array, true).'</pre>';
+    }
 
-		return $this;
-	}
+    /**
+     * @param $index
+     * @return mixed|null
+     */
+    public function get($index) {
+        if(array_key_exists($index, $this->array))
+            return $this->array[$index];
+        else
+            return null;
+    }
 
-	/**
-	 * @param $value
-	 * @return $this
-	 */
-	public function add($value) {
-		$this->array[] = $value;
+    /**
+     * @param $index
+     * @param $value
+     * @return $this
+     */
+    public function set($index, $value) {
+        $this->array[$index] = $value;
+        $this->keys[] = $index;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isEmpty() {
-		return empty($this->array);
-	}
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function add($value) {
+        $this->array[] = $value;
+        $this->keys[] = count($this->array) - 1;
 
-	/**
-	 * @return int
-	 */
-	public function count() {
-		return count($this->array);
-	}
+        return $this;
+    }
 
-	/**
-	 * @return array
-	 */
-	public function toArray() {
-		return $this->array;
-	}
-	
+    /**
+     * @return bool
+     */
+    public function isEmpty() {
+        return empty($this->array);
+    }
+
+    /**
+     * @return int
+     */
+    public function count() {
+        return count($this->array);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray() {
+        return $this->array;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function current()
+    {
+        $key = $this->keys[$this->iteratrionPosition];
+
+        return $this->array[$key];
+    }
+
+    /**
+     * Get next key
+     *
+     * @return void
+     */
+    public function next()
+    {
+        $this->iteratrionPosition++;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function key()
+    {
+        return $this->keys[$this->iteratrionPosition];
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        $key = $this->keys[$this->iteratrionPosition];
+
+        return isset($this->array[$key]);
+    }
+
+    /**
+     * Reset position after iteration finished.
+     *
+     * @return void
+     */
+    public function rewind()
+    {
+        $this->iteratrionPosition = 0;
+    }
 }
